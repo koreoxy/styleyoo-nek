6cfc1cd6f4a881d5b2b8086c3260c40a06bc2d6e
koreoxy <rayssankn@gmail.com>
2024-03-17 02:10:01 +0700

    fix : navbar for mobile layout

--- src/components/site/Navbar.tsx
+++ src/components/site/Navbar.tsx
@@ -4,10 +4,10 @@ import { Link } from "react-router-dom";
 
 export const Navbar = () => {
   return (
-    <nav className="w-full sticky top-0 z-50 border-b bg-white">
+    <nav className="w-full sticky top-0 z-50 border-b bg-white p-5 sm:p-0">
       <div className="flex h-16 items-center px-10 sm:px-16 lg:px-44">
         <div className="mx-auto w-full max-w-3xl space-y-20">
-          <div className="flex justify-between">
+          <div className="flex flex-col items-center sm:flex-row justify-between">
             <div className="flex flex-1 items-center justify-start">
               <Link
                 to="/"
@@ -16,7 +16,7 @@ export const Navbar = () => {
                 Styleyoo/nek
               </Link>
             </div>
-            <div className="flex flex-1 items-center justify-end gap-4">
+            <div className="flex flex-1 items-center justify-end gap-4 mt-2 sm:mt-0">
               <div className="flex items-center gap-5">
                 <Link to="/about">About</Link>
                 <Link to="/">Docs</Link>
